ffb2022ad2aef167c28336f43a72ac9ed9dc184b
Mitu217 <mtksk.26884um@gmail.com>
2019-03-16 17:39:07 +0900

    Fix SetSchema and SetRows

--- connection.go
+++ connection.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"fmt"
 	"reflect"
 	"strings"
-	"time"
 
 	"github.com/Mitu217/tamate/driver"
 )
@@ -50,92 +48,30 @@ func (c *mysqlConn) Close() error {
 	return nil
 }
 
-func (c *mysqlConn) GetSchema(ctx context.Context, name string) (*driver.Schema, error) {
-	schemaMap, err := c.getSchemaMap()
+func (c *mysqlConn) GetSchema(ctx context.Context, tableName string) (*driver.Schema, error) {
+	rows, err := getInfomationSchemaDB(c.db, tableName)
 	if err != nil {
 		return nil, err
 	}
-	for scName, sc := range schemaMap {
-		if scName == name {
-			return sc, nil
+	defer rows.Close()
+
+	var schema *driver.Schema
+	for rows.Next() {
+		if schema == nil {
+			schema = &driver.Schema{Name: tableName}
 		}
-	}
-	return nil, errors.New("schema not found: " + name)
-}
 
-func (c *mysqlConn) SetSchema(ctx context.Context, name string, schema *driver.Schema) error {
-	return fmt.Errorf("feature support")
-}
-
-func (c *mysqlConn) GetRows(ctx context.Context, name string) ([]*driver.Row, error) {
-	result, err := c.db.Query(fmt.Sprintf("SELECT * FROM %s", name))
-	if err != nil {
-		return nil, err
-	}
-	defer result.Close()
-
-	/*
-		var rows []*driver.Row
-		for result.Next() {
-			rowValues := make(driver.RowValues, len(schema.Columns))
-			rowValuesGroupByKey := make(driver.GroupByKey)
-			ptrs := make([]interface{}, len(schema.Columns))
-			for i, col := range schema.Columns {
-				ptr := reflect.New(colToMySQLType(col)).Interface()
-				ptrs[i] = ptr
-			}
-			if err := result.Scan(ptrs...); err != nil {
-				return nil, err
-			}
-			for i, col := range schema.Columns {
-				val := reflect.ValueOf(ptrs[i]).Elem().Interface()
-				colValue := &driver.GenericColumnValue{Column: col, Value: val}
-				rowValues[col.Name] = colValue
-				for i := range schema.PrimaryKey.ColumnNames {
-					if schema.PrimaryKey.ColumnNames[i] == col.Name {
-						key := schema.PrimaryKey.String()
-						rowValuesGroupByKey[key] = append(rowValuesGroupByKey[key], colValue)
-					}
-				}
-			}
-			rows = append(rows, &driver.Row{GroupByKey: rowValuesGroupByKey, Values: rowValues})
-		}
-		return rows, nil
-	*/
-	return nil, nil
-}
-
-func (c *mysqlConn) SetRows(ctx context.Context, name string, rows []*driver.Row) error {
-	return fmt.Errorf("feature support")
-}
-
-func (c *mysqlConn) getSchemaMap() (map[string]*driver.Schema, error) {
-	// get schemas
-	sqlRows, err := c.db.Query("SELECT TABLE_NAME, COLUMN_NAME, ORDINAL_POSITION, COLUMN_TYPE, COLUMN_KEY, IS_NULLABLE, EXTRA FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = DATABASE()")
-	if err != nil {
-		return nil, err
-	}
-	defer sqlRows.Close()
-
-	// scan results
-	schemaMap := make(map[string]*driver.Schema)
-	for sqlRows.Next() {
-		var tableName string
 		var columnName string
 		var ordinalPosition int
 		var columnType string
 		var columnKey string
 		var isNullable string
 		var extra string
-		if err := sqlRows.Scan(&tableName, &columnName, &ordinalPosition, &columnType, &columnKey, &isNullable, &extra); err != nil {
+		if err := rows.Scan(&columnName, &ordinalPosition, &columnType, &columnKey, &isNullable, &extra); err != nil {
 			return nil, err
 		}
-		// prepare schema
-		if _, ok := schemaMap[tableName]; !ok {
-			schemaMap[tableName] = &driver.Schema{Name: tableName}
-		}
-		schema := schemaMap[tableName]
-		// set column in schema
+
+		// key
 		if strings.Contains(columnKey, "PRI") {
 			if schema.PrimaryKey == nil {
 				schema.PrimaryKey = &driver.Key{
@@ -144,6 +80,8 @@ func (c *mysqlConn) getSchemaMap() (map[string]*driver.Schema, error) {
 			}
 			schema.PrimaryKey.ColumnNames = append(schema.PrimaryKey.ColumnNames, columnName)
 		}
+
+		// column
 		ct, err := columnTypeFromMySQLToGeneric(columnType)
 		if err != nil {
 			return nil, err
@@ -156,8 +94,75 @@ func (c *mysqlConn) getSchemaMap() (map[string]*driver.Schema, error) {
 			AutoIncrement:   strings.Contains(extra, "auto_increment"),
 		}
 		schema.Columns = append(schema.Columns, column)
-		schemaMap[tableName] = schema
 	}
 
-	return schemaMap, nil
+	if schema == nil {
+		return nil, errors.New("schema not found: " + tableName)
+	}
+	return schema, nil
+}
+
+func (c *mysqlConn) SetSchema(ctx context.Context, tableName string, sc *driver.Schema) error {
+	dropTableDB(c.db, tableName)
+	createTableDB(c.db, sc)
+	return nil
+}
+
+func (c *mysqlConn) GetRows(ctx context.Context, tableName string) ([]*driver.Row, error) {
+	schema, err := c.GetSchema(ctx, tableName)
+	if err != nil {
+		return nil, err
+	}
+
+	resultRows, err := selectRowsDB(c.db, tableName)
+	if err != nil {
+		return nil, err
+	}
+	defer resultRows.Close()
+
+	var rows []*driver.Row
+	for resultRows.Next() {
+		rowValues := make(driver.RowValues, len(schema.Columns))
+		rowValuesGroupByKey := make(driver.GroupByKey)
+		ptrs := make([]interface{}, len(schema.Columns))
+		for i, col := range schema.Columns {
+			ptr := reflect.New(colToMySQLType(col)).Interface()
+			ptrs[i] = ptr
+		}
+		if err := resultRows.Scan(ptrs...); err != nil {
+			return nil, err
+		}
+		for i, col := range schema.Columns {
+			val := reflect.ValueOf(ptrs[i]).Elem().Interface()
+			colValue := &driver.GenericColumnValue{Column: col, Value: val}
+			rowValues[col.Name] = colValue
+			for i := range schema.PrimaryKey.ColumnNames {
+				if schema.PrimaryKey.ColumnNames[i] == col.Name {
+					key := schema.PrimaryKey.String()
+					rowValuesGroupByKey[key] = append(rowValuesGroupByKey[key], colValue)
+				}
+			}
+		}
+		rows = append(rows, &driver.Row{GroupByKey: rowValuesGroupByKey, Values: rowValues})
+	}
+	return rows, nil
+}
+
+func (c *mysqlConn) SetRows(ctx context.Context, tableName string, rows []*driver.Row) error {
+	sc, err := c.GetSchema(ctx, tableName)
+	if err != nil {
+		return err
+	}
+
+	dropTableDB(c.db, tableName)
+	createTableDB(c.db, sc)
+
+	for _, row := range rows {
+		_, err := insertRowDB(c.db, tableName, row)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
--- connection_test.go
+++ connection_test.go
@@ -54,6 +54,63 @@ func Test_GetSchema(t *testing.T) {
 	}
 }
 
+func Test_SetSchema(t *testing.T) {
+	var (
+		ctx       = context.Background()
+		dbName    = "tamatest"
+		tableName = "example"
+		dsn       = fmt.Sprintf("%s:%s@/%s", ConnectionTestUser, ConnectionTestPassword, dbName)
+	)
+
+	// Prepare test
+	fakeSchema := &driver.Schema{
+		Name: tableName,
+		PrimaryKey: &driver.Key{
+			KeyType:     driver.KeyTypePrimary,
+			ColumnNames: []string{"id"},
+		},
+		Columns: []*driver.Column{
+			driver.NewColumn("id", 0, driver.ColumnTypeInt, true, false),
+			driver.NewColumn("name", 1, driver.ColumnTypeString, true, false),
+		},
+	}
+	assert.NoError(t, dropDatabase(ConnectionTestUser, ConnectionTestPassword, dbName))
+	assert.NoError(t, createDatabase(ConnectionTestUser, ConnectionTestPassword, dbName))
+
+	// Open datasource
+	ds, err := tamate.Open("mysql", dsn)
+	defer ds.Close()
+	assert.NoError(t, err)
+
+	// Setting schema
+	assert.NoError(t, ds.SetSchema(ctx, tableName, fakeSchema))
+	rows, err := getInformationSchema(ConnectionTestUser, ConnectionTestPassword, dbName, tableName)
+	if assert.NoError(t, err) {
+		for rows.Next() {
+			var columnName string
+			var ordinalPosition int
+			var columnType string
+			var columnKey string
+			var isNullable string
+			var extra string
+			assert.NoError(t, rows.Scan(&columnName, &ordinalPosition, &columnType, &columnKey, &isNullable, &extra))
+			switch columnName {
+			case "id":
+				assert.Equal(t, 1, ordinalPosition)
+				assert.Equal(t, "int(11)", columnType)
+				assert.Equal(t, "PRI", columnKey)
+				assert.Equal(t, "NO", isNullable)
+				assert.Equal(t, "", extra)
+			case "name":
+				assert.Equal(t, 2, ordinalPosition)
+				assert.Equal(t, "text", columnType)
+				assert.Equal(t, "", columnKey)
+				assert.Equal(t, "NO", isNullable)
+				assert.Equal(t, "", extra)
+			default:
+				t.Errorf("undefined column: %s", columnName)
+			}
+		}
 	}
 }
 
@@ -123,5 +180,62 @@ func Test_GetRows(t *testing.T) {
 	}
 }
 
+func Test_SetRows(t *testing.T) {
+	var (
+		ctx       = context.Background()
+		dbName    = "tamatest"
+		tableName = "example"
+		dsn       = fmt.Sprintf("%s:%s@/%s", ConnectionTestUser, ConnectionTestPassword, dbName)
+	)
+
+	// Prepare test
+	fakeSchema := &driver.Schema{
+		Name: tableName,
+		PrimaryKey: &driver.Key{
+			KeyType:     driver.KeyTypePrimary,
+			ColumnNames: []string{"id"},
+		},
+		Columns: []*driver.Column{
+			driver.NewColumn("id", 0, driver.ColumnTypeInt, true, false),
+			driver.NewColumn("name", 1, driver.ColumnTypeString, true, false),
+		},
+	}
+	fakeRows := []*driver.Row{
+		&driver.Row{
+			Values: map[string]*driver.GenericColumnValue{
+				"id": &driver.GenericColumnValue{
+					Column: driver.NewColumn("id", 0, driver.ColumnTypeInt, true, false),
+					Value:  1,
+				},
+				"name": &driver.GenericColumnValue{
+					Column: driver.NewColumn("name", 1, driver.ColumnTypeString, true, false),
+					Value:  "user",
+				},
+			},
+		},
+	}
+	assert.NoError(t, dropDatabase(ConnectionTestUser, ConnectionTestPassword, dbName))
+	assert.NoError(t, createDatabase(ConnectionTestUser, ConnectionTestPassword, dbName))
+	assert.NoError(t, dropTable(ConnectionTestUser, ConnectionTestPassword, dbName, tableName))
+	assert.NoError(t, createTable(ConnectionTestUser, ConnectionTestPassword, dbName, fakeSchema))
+
+	// Open datasource
+	ds, err := tamate.Open("mysql", dsn)
+	defer ds.Close()
+	assert.NoError(t, err)
+
+	// Setting rows
+	if assert.NoError(t, ds.SetRows(ctx, tableName, fakeRows)) {
+		rows, err := selectRows(ConnectionTestUser, ConnectionTestPassword, dbName, tableName)
+		assert.NoError(t, err)
+		defer rows.Close()
+
+		for rows.Next() {
+			var id int64
+			var name string
+			assert.NoError(t, rows.Scan(&id, &name))
+			assert.Equal(t, int64(1), id)
+			assert.Equal(t, "user", name)
+		}
 	}
 }
